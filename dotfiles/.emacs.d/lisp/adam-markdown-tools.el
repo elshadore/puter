c35ab33937e529e41645910f581e9b2b7891430c
adam <elshadore@proton.me>
2026-09-01 06:41:39 +0100

    emac 4

--- dotfiles/.emacs.d/lisp/adam-markdown-tools.el
+++ dotfiles/.emacs.d/lisp/adam-markdown-tools.el
@@ -21,13 +21,34 @@
      (1 'adam/markdown-hide-hash prepend)
      (2 nil prepend))))
 
+(defconst adam/markdown-link-invisible 'adam-markdown-link-hide)
+
+(defun adam/markdown-fontify-inline-links (last)
+  "Fontify inline links to hide URL and markup, showing only link text."
+  (when (markdown-match-generic-links last nil)
+    (dolist (g '(2 4 5 6 8))
+      (when (match-end g)
+        (put-text-property (match-beginning g) (match-end g)
+                           'invisible adam/markdown-link-invisible)))
+    t))
+
+(defconst adam/markdown-hide-link-keywords
+  '((adam/markdown-fontify-inline-links)))
+
 ;;;###autoload
 (define-minor-mode adam-markdown-tools-mode
   "Minor mode for some custom tools and additions to the default `markdown-mode' experiance."
   :lighter " adam-markdown-tools"
   (if adam-markdown-tools-mode
-      (font-lock-add-keywords nil adam/markdown-hide-hash-keywords 'append)
-    (font-lock-remove-keywords nil adam/markdown-hide-hash-keywords)))
+      (progn
+        (add-to-invisibility-spec adam/markdown-link-invisible)
+        (font-lock-add-keywords nil adam/markdown-hide-hash-keywords 'append)
+        (font-lock-add-keywords nil adam/markdown-hide-link-keywords 'append))
+    (remove-from-invisibility-spec adam/markdown-link-invisible)
+    (font-lock-remove-keywords nil adam/markdown-hide-hash-keywords)
+    (font-lock-remove-keywords nil adam/markdown-hide-link-keywords))
+  (when font-lock-mode
+    (font-lock-flush)))
 
 (provide 'adam-markdown-tools)
 ;;; adam-markdown-tools.el ends here
